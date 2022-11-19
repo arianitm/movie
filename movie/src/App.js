a29579c8f259637816ba658e107bcb778f1c6cbf
Arianit <80793389+arianitm@users.noreply.github.com>
2022-11-19 20:13:45 +0100

    Create movie folders and call api for movies

--- movie/src/App.js
+++ movie/src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import "./App.scss";
+import Footer from "./components/Footer/Footer";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
 
 function App() {
   return (
-   <div>
-    Hello
+    <div className="app">
+      <Header />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movie:imdbID" element={<MovieDetail />} />
+          <Route path="" element={<PageNotFound />} />
+        </Routes>
+      </div>
+      <Footer />
     </div>
   );
 }
